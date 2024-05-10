dc79eb665ac7aadedfe30de11f32a5579eed0ec0
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-05-10 11:34:49 +0200

    minor bug fix

--- Project 6. Sort/src/Quick.java
+++ Project 6. Sort/src/Quick.java
@@ -1,7 +1,7 @@
 public class Quick {
 
     public static void sort(Comparable[] a) {
-        StdRandom.shuffle(a);  // Shuffle to guarantee performance
+        StdRandom.shuffle(a); 
         sort(a, 0, a.length - 1);
     }
 
